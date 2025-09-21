9fba091ad2cf6cdb9d6b55abf0c02e6989db29e5
WindowScary321 <126940686+WindowScary321@users.noreply.github.com>
2025-09-21 19:57:28 +0700

    smol update 2

---
+++ src/main/java/onClass/stringBuilder.java
@@ -0,0 +1,17 @@
+package onClass;
+
+import java.util.Locale;
+import  java.util.Scanner;
+
+public class stringBuilder {
+    public static void main(String[] args) {
+        String s = "Hello 1 @ World. & 2 How * 4 Are today. Today is Monday.";
+        StringBuilder stringBuilder = new StringBuilder(s);
+
+
+
+        System.out.println(s);
+
+
+    }
+}
--- src/main/java/onClass/stringChar.java
+++ src/main/java/onClass/stringChar.java
@@ -1,5 +1,6 @@
 package onClass;
 
+import java.util.Locale;
 import  java.util.Scanner;
 
 public class stringChar {
@@ -7,15 +8,18 @@ public class stringChar {
 //        String s = "Hôm nay trời đẹp quá và tôi có 300000000000đ trong ví.";
         String s = "Hello 1 @ World.           & 2 How * 4 Are today. Today is Monday.";
         String s1 = " ";
-        String[] arrWorld = s.split("\\s");
+//        String[] arrWorld = s.split("\\s");
 
-//        s = s.concat(s);
+//        System.out.println(s.substring(0,11)); // in kí tự từ 0 -> 11
+//        System.out.println(s.substring(13)); // in 13
+//        System.out.println(s.toUpperCase()); //wells in hoa :v
+//        s = s.concat(s); đọc kí tự từ output :v
 //        s = s.replace(".",","); // thay kí tự . -> s
-//
- //        s = s.replaceAll("\\w", "yay"); // nhớ dùng //'kí tự regex'
 
-        System.out.println(s);
-        System.out.println(arrWorld.length);
+ //        s = s.replaceAll("\\w", "yay"); // nhớ dùng //'kí tự regex'
+//
+//        System.out.println(s);
+//        System.out.println(arrWorld.length);
 
 //        System.out.println(s.contains(s1));
 //        System.out.println(s.compareTo(s1));
@@ -25,7 +29,10 @@ public class stringChar {
 //        System.out.println(s1.isBlank());
 //        System.out.println(s1.isEmpty());
 
-
+//        char[] arrChars = s.toCharArray();
+//        for(char c:arrChars){
+//            System.out.println(c);
+//        }
 
 
     }
