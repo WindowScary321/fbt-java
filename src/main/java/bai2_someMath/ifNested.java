ddcf4674278e1109875f94276fcd8dd49b603827
WindowScary321 <126940686+WindowScary321@users.noreply.github.com>
2025-09-23 20:43:11 +0700

    New oop commit!

---
+++ src/main/java/bai2_someMath/ifNested.java
@@ -0,0 +1,29 @@
+package bai2_someMath;
+
+public class ifNested {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 10;
+        String discountMessage = "";
+
+        if (isStudent) {
+            if (isSenior) {
+                price *= 0.7;
+                discountMessage = "Bạn là học sinh và người cao tuổi, được giảm giá 30%";
+            } else {
+                price *= 0.9;
+                discountMessage = "Bạn là học sinh, được giảm giá 10%";
+            }
+        } else {
+            if (isSenior) {
+                price *= 0.8;
+                discountMessage = "Bạn là người cao tuổi, được giảm giá 20%";
+            } else {
+                discountMessage = "Giá vé gốc là";
+            }
+        }
+
+        System.out.printf("%s: %.2f", discountMessage, price);
+    }
+}
---
+++ src/main/java/bai3_oopStuffs/Main.java
@@ -0,0 +1,4 @@
+package bai3_oopStuffs;
+
+public class Main {
+}
