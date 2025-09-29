249d159a2f6dc40718af329869596ee3cbe01e08
WindowScary321 <126940686+WindowScary321@users.noreply.github.com>
2025-09-29 11:48:48 +0700

    Cleanup stuffs

--- src/main/java/BasicStuffs/bai3_oopStuffs/Utils.java
+++ src/main/java/BasicStuffs/bai3_oopStuffs/Utils.java
@@ -23,31 +23,3 @@ public class Utils {
         return num;
     }
 }
-
-//package BasicStuffs.bai3_oopStuffs;
-//
-//import java.util.Scanner;
-//
-//public class Utils {
-//    Scanner scanner = new Scanner(System.in);
-//    public int inputInt(String msg, int min, int max){
-//        int num;
-//        do{
-//            System.out.println(msg);
-//            try { // lặp liên tục, nếu lỗi nhảy vào "catch"
-//                num=Integer.parseInt(scanner.nextLine()); // num = scanner.nextInt();
-//                if (!(num>=min && num<=max)){
-//                    System.out.println("Out of rage, vui lòng nhập lại.");
-//                }else{
-//                    break;
-//                }
-//                break; // user nhập đúng -> thoát khỏi vòng lặp
-//            }catch (Exception e){
-//                System.out.println("Sai kiểu dữu liệu!!! Vui lòng thử lại.");
-//                scanner.nextLine();
-//            }
-//        }while (true); // lặp liên tục
-//        return num;
-//
-//    }
-//}
